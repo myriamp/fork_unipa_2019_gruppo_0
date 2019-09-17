585f33b0a68741bdf0587260b4d1a69c4185f46d
Crooch <christian.scaccia@gmail.com>
2019-09-17 15:17:42 +0200

    v1.2

---
+++ filesharing-backend/src/main/java/it/eng/unipa/filesharing/repository/TokenUtenteRepository.java
@@ -0,0 +1,15 @@
+package it.eng.unipa.filesharing.repository;
+
+import it.eng.unipa.filesharing.model.TokenUtente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TokenUtenteRepository extends JpaRepository<TokenUtente>{
+    @Query("select tu from TokenUtente tu ")
+    List<TokenUtente> utentiRichiestaNotifiche(@Param("email")String email);
+}
